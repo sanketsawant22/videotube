9da963c2d533357490d6d3b4258cdcbb924a47a9
Sanket Sawant <sanketsawant2210@gmail.com>
2024-12-15 00:57:51 +0530

    data models

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -1,23 +1,20 @@
 import mongoose, { Schema } from "mongoose";
 
-const commentSchema = new Schema(
+const tweetSchema = new Schema(
     {
         content: {
             type: String,
-            required: true
-        },
-        video: {
-            type: Schema.Types.ObjectId,
-            ref: 'Video',
+            required: true,
         },
         owner: {
             type: Schema.Types.ObjectId,
             ref: "User",
         },
+        
     },
     {
         timestamps: true
     }
 );
 
-export const Comment = mongoose.model("Comment", commentSchema);
+export const Tweet = mongoose.model("Tweet", tweetSchema);
