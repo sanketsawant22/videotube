b039861c82055454a27d113b9be629af5b00570e
Sanket Sawant <sanketsawant2210@gmail.com>
2024-12-15 00:58:38 +0530

    #

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 
+
 const app = express();
 
 // cors : cross origin resource sharing
 // it handles who should be allowed to access the database
 
+
+// cors middleware
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
@@ -13,8 +16,15 @@ app.use(
     })
 );
 
+// common middleware
 app.use(express.json({limit : "16kb"}));       // express middleware
 
+// import routes
+import router from './routes/healthcheck.routes.js';
 
+
+
+// routes
+app.use("/api/v1/healthcheck", router)
 
 export {app};
